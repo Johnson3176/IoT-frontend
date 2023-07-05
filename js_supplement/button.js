edba66c331c1de9c4a3ac3e3753b10fdfc0b838b
Johnson3176 <cschaozheyu@gmail.com>
2023-07-05 14:26:59 +0800

    暑假同步

--- js_supplement/button.js
+++ js_supplement/button.js
@@ -1,8 +1,10 @@
 // 定义全局变量
 
 var is_on = false;
+var acquire_data = false;
 var IntervalTime = 1000;
 var auto_control = 1;
+
 
 function setScale() {
   let designWidth = 1440; //设计稿的宽度，根据实际项目调整
@@ -24,32 +26,99 @@ function updateData() {
 function boilControl() {
   bton = document.getElementById("曝气控制");
   // 曝气控制信号发送
-  $.ajax({
-    url:'http://192.168.92.178:5000/test',  //后台接口地址
-    type:'post',  //post请求方式
-    async: true,
-    dataType:'json',
-    contentType:'application/json;charset=utf-8',
-    data:JSON.stringify({"LED0_STATE":1,"LED1_STATE":0,"LED2_STATE":1,"LED3_STATE":0,
-                         "LED4_STATE":0,"LED5_STATE":0,"LED6_STATE":0,"LED7_STATE":0}),
-    success:function(data){
-      alert(data);
-      console.log('请求成功')
-    },
-    error:function(){
-
-    }
-  })
+  
 }
 
 function onlineDetection() {
-  bton = document.getElementById("在线监测");
+  $.ajax({
+    url:'http://192.168.64.219:5000',
+    type:'get',  //get请求方式
+    async: true,
+    dataType:'json',
+    contentType:'application/json;charset=utf-8',
+  })
+  bton = document.getElementById("在线曝气");
   if (!is_on) {
-    bton.innerHTML = "暂停监测";
+    bton.innerHTML = "暂停曝气";
+    $.ajax({
+      url:'http://192.168.64.219:5000/ctrl_motor2/1',  //后台接口地址
+      type:'get',  //get请求方式
+      async: true,
+      dataType:'json',
+      contentType:'application/json;charset=utf-8',
+      success:function(data){
+        // alert(data);
+        console.log('请求成功')
+      },
+      error:function(){
+  
+      }
+    })
+    $.ajax({
+      url:'http://192.168.64.219:5000/ctrl_motor3/1',  //后台接口地址
+      type:'get',  //get请求方式
+      async: true,
+      dataType:'json',
+      contentType:'application/json;charset=utf-8',
+      success:function(data){
+        // alert(data);
+        console.log('请求成功')
+      },
+      error:function(){
+  
+      }
+    })
     is_on = true;
   } else {
-    bton.innerHTML = "继续监测";
+    bton.innerHTML = "继续曝气";
     is_on = false;
+    $.ajax({
+      url:'http://192.168.64.219:5000/ctrl_motor2/0',  //后台接口地址
+      type:'get',  //get请求方式
+      async: true,
+      dataType:'json',
+      contentType:'application/json;charset=utf-8',
+      success:function(data){
+        // alert(data);
+        console.log('请求成功')
+      },
+      error:function(){
+  
+      }
+    })
+    $.ajax({
+      url:'http://192.168.64.219:5000/ctrl_motor3/0',  //后台接口地址
+      type:'get',  //get请求方式
+      async: true,
+      dataType:'json',
+      contentType:'application/json;charset=utf-8',
+      success:function(data){
+        // alert(data);
+        console.log('请求成功')
+      },
+      error:function(){
+  
+      }
+    })
+  }
+}
+
+function acquireData(){
+  bton = document.getElementById("获取数据");
+  $.ajax({
+    url:'http://192.168.64.219:5000/start',
+    type:'get',  //get请求方式
+    async: true,
+    dataType:'json',
+    contentType:'application/json;charset=utf-8',
+  })
+  if(!acquire_data){
+    acquire_data=true;
+    bton.innerHTML = "暂停获取";
+  }
+  else{
+    acquire_data=false;
+    bton.innerHTML = "继续获取";
   }
 }
 
@@ -62,6 +131,13 @@ window.onresize = function () {
 
 // 通过 jQuery，您可以选取（查询，query） HTML 元素，并对它们执行"操作"（actions）。
 $(document).ready(function () {
+  $.ajax({
+    url:'http://192.168.64.219:5000',
+    type:'get',  //get请求方式
+    async: true,
+    dataType:'json',
+    contentType:'application/json;charset=utf-8',
+  })
   $(function () {
     let text = $("#fTbody:first");
     let status = $("#在线监测");
@@ -76,7 +152,7 @@ $(document).ready(function () {
     controlInterval = setInterval(function () {
       if (auto_control)
         $.ajax({
-          url: "http://192.168.64.219:5000", //后台请求的数据
+          url: "http://192.168.43.12:5000", //后台请求的数据
           dataType: "json", // 预期返回的数据类型，如果是json格式，在接收到返回时会自动封装成json对象
           type: "get", // 请求方式
           async: true, // 是否异步请求
@@ -107,31 +183,33 @@ $(document).ready(function () {
         function () {
           // 请求&页面更新数据，用两个计时器实现，鼠标移入只会暂停页面更新数据
           requestInterval = setInterval(function () {
-            if (is_on)
+            if (acquire_data)
               $.ajax({
-                url: "http://192.168.43.183:5000/query", //后台请求的数据
+                url: "http://192.168.64.219:5000/query", //后台请求的数据
                 dataType: "json", // 预期返回的数据类型，如果是json格式，在接收到返回时会自动封装成json对象
                 type: "get", // 请求方式
                 async: true, // 是否异步请求
                 success: function (data) {
+                  console.log(data)
                   let td;
-                  td = "<td>" + data.time + "</td>";
-                  td = "<td>" + data.temperature + "</td>";
-                  td = "<td>" + data.DO + "</td>";
-                  td = "<td>" + data.PH + "</td>";
-                  td = "<td>" + data.TDS + "</td>";
+                  td = "<td>" + data.datetime + "</td>";
+                  td += "<td>" + data.temperature + "</td>";
+                  td += "<td>" + data.DO + "</td>";
+                  td += "<td>" + data.PH + "</td>";
+                  td += "<td>" + data.TDS + "</td>";
                   tr_tmp1 = tr_tmp2;
                   tr_tmp2 = tr_tmp3;
                   tr_tmp3 = "<tr>" + td + "</tr>";
+                  console.log(tr_tmp3);
                 },
                 error: function (arg1) {
                   console.log(arg1);
-                  alert("加载数据失败");
+                  // alert("加载数据失败");
                 },
               });
           }, IntervalTime);
           updateInterval = setInterval(function () {
-            if (is_on) text.html(tr_tmp1 + tr_tmp2 + tr_tmp3);
+            if (acquire_data) text.html(tr_tmp1 + tr_tmp2 + tr_tmp3);
           }, IntervalTime); // 滚动间隔时间
         }
       )
